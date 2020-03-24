fb66b334df19ed1ba0b8aa25a3974ff8fa692d1b
Zheyong Fan <brucenju@gmail.com>
2020-03-24 23:31:55 +0200

    update figure

--- examples/review/square/weak-localization/plot_results.m
+++ examples/review/square/weak-localization/plot_results.m
@@ -62,12 +62,16 @@ ylabel('$\sigma_{max}$ ($e^2/h$)','fontsize',font_size,'interpreter','latex');
 set(gca,'fontsize',font_size,'ticklength',get(gca,'ticklength')*2);
 
 figure;
-semilogx(len(:,1001),sigma_from_msd(:,1001),'rs','linewidth',2);
+plot(len(:,1001),sigma_from_msd(:,1001),'rs','linewidth',2);
 hold on;
 plot(len(:,751),(sigma_from_msd(:,751)+sigma_from_msd(:,1251))/2,'bo','linewidth',2);
 x=1:200;
 plot(x,sigma_sc(1001)-2/pi*log(x/len(index(1001),1001)),'r-');
 plot(x,sigma_sc(751)-2/pi*log(x/len(index(751),751)),'b--');
+plot(1:34,14.58*ones(1,34),'k--');
+plot(34*ones(1,16),0:15,'k--');
+text(30,-1,'$l_{\rm sc}$','interpreter','latex','fontsize',18);
+text(-20,15,'$\sigma_{\rm sc}$','interpreter','latex','fontsize',18);
 ylim([0.1,20]);
 legend('E = 0 (numerical)','E = \gamma (numerical)','E = 0 (theory)','E = \gamma (theory)');
 xlabel('$L$ (a)', 'fontsize',font_size,'interpreter','latex');
